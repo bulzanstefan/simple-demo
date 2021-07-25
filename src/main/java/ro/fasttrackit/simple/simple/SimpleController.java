c234cb4ece3a7b99fd884dab76b498465cd71d87
Bulzan Stefan <stefan.bulzan@nordlogic.com>
2021-07-26 01:23:21 +0300

    test

--- src/main/java/ro/fasttrackit/simple/simple/SimpleController.java
+++ src/main/java/ro/fasttrackit/simple/simple/SimpleController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @GetMapping
     String simple() {
-        return "simple-new";
+        return "simple-test";
     }
 }
